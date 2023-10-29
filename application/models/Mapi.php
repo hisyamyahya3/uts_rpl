2f5d0e4ae6de15d163d8280e9c4019f1702969a5
Kito <surya.aw26@gmail.com>
2023-10-29 16:37:07 +0700

    fix delete function

--- application/models/Mapi.php
+++ application/models/Mapi.php
@@ -1,73 +1,84 @@
 <?php
-class Mapi extends CI_Model {
+class Mapi extends CI_Model
+{
 
-    function get_data($obj) {
-        if(array_key_exists('filter',$obj)) {
-            $data = $this->db->query("select * from koleksifilmku where $obj->filter")->result();
-        }else{
-            $data = $this->db->query("select * from koleksifilmku")->result();
+    function get_data($obj)
+    {
+        if (array_key_exists('filter', $obj)) {
+            $data = $this->db->query("SELECT * FROM koleksifilmku WHERE $obj->filter")->result();
+        } else {
+            $data = $this->db->query("SELECT * FROM koleksifilmku")->result();
         }
 
         $hasil = array(
             'status' => 'ok',
             'data' => $data
         );
+
         return json_encode($hasil);
     }
 
-    
-    function input_data($obj) {
+
+    function input_data($obj)
+    {
         $data = $obj->data;
-        $input = $this->db->insert('koleksifilmku',$data);
-        if($input) {
+        $input = $this->db->insert('koleksifilmku', $data);
+
+        if ($input) {
             $hasil = array(
                 'status' => 'ok',
                 'keterangan' => 'data berhasil ditambahkan'
             );
-        }else {
+        } else {
             $hasil = array(
                 'status' => 'gagal',
                 'keterangan' => 'data gagal ditambahkan'
-            );   
+            );
         }
+
         return json_encode($hasil);
     }
 
-    function update_data($obj) {
+    function update_data($obj)
+    {
         $data = $obj->data;
-        $this->db->where('id',$obj->id);
-        $update = $this->db->update('koleksifilmku',$data);
-        if($update) {
+        $this->db->where('id', $obj->id);
+        $update = $this->db->update('koleksifilmku', $data);
+
+        if ($update) {
             $hasil = array(
                 'status' => 'ok',
                 'keterangan' => 'data berhasil diupdate'
             );
-        }else {
+        } else {
             $hasil = array(
                 'status' => 'gagal',
                 'keterangan' => 'data gagal diupdate'
-            );   
+            );
         }
+
         return json_encode($hasil);
     }
 
-    function delete_data($obj) {
-        $data = $obj->data;
-        $this->db->where('id', $obj->id);
-        $delete = $this->db->delete('koleksifilmku');
-        // $delete = $this->db->delete('koleksifilmku', array('id' => $data->id));
-        if($delete) {
+    function delete_data($obj)
+    {
+        $request = $obj->data;
+        $data = $this->db->query("SELECT * FROM koleksifilmku WHERE id = $request->id")->row();
+
+        if ($data) {
+            $this->db->query("DELETE FROM koleksifilmku WHERE id = $request->id");
+
             $hasil = array(
                 'status' => 'ok',
                 'keterangan' => 'data berhasil dihapus'
             );
-        }else {
+        } else {
             $hasil = array(
                 'status' => 'gagal',
                 'keterangan' => 'data gagal dihapus'
-            );   
+            );
         }
+
         return json_encode($hasil);
     }
-
 }
